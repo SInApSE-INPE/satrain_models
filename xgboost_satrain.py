70e32f5f228dd2ebaf662a148df698c34dcb76df
thaina <thaina.lopes@autbank.com.br>
2026-01-08 11:48:32 -0300

    Atualização

--- xgboost_satrain.py
+++ xgboost_satrain.py
@@ -2,12 +2,11 @@ import numpy as np
 import torch
 import xgboost as xgb
 from torch.utils.data import DataLoader
-from joblib import Parallel, delayed
-from ipwgml.input import GMI,Ancillary, Geo, GeoIR
-from ipwgml.target import TargetConfig
-from ipwgml.pytorch.datasets import SatRainTabular
-from ipwgml.pytorch import PytorchRetrieval
-from ipwgml.evaluation import Evaluator
+from satrain.input import GMI
+from satrain.target import TargetConfig
+from satrain.pytorch.datasets import SatRainTabular
+from satrain.pytorch import PytorchRetrieval
+from satrain.evaluation import Evaluator
 
 SEED = 42
 np.random.seed(SEED)
@@ -16,46 +15,48 @@ torch.cuda.manual_seed_all(SEED)
 
 inputs = [GMI(normalize="minmax", nan=-1.5, include_angles=False)]
 target_config = TargetConfig(min_rqi=0.5)
-ipwgml_path = "/storage/satrain"
+satrain_path = "/prj/cptec"
+geometry = "gridded"
 batch_size = 1024
+subset = "xs"
 
 training_data = SatRainTabular(
     base_sensor="gmi",
-    geometry="gridded",
+    geometry=geometry,
     split="training",
-    subset="xs",
+    subset=subset,
     retrieval_input=inputs,
     batch_size=batch_size,
     target_config=target_config,
     stack=True,
-    ipwgml_path=ipwgml_path,
+    data_path=satrain_path,
     download=False,
 )
 validation_data = SatRainTabular(
     base_sensor="gmi",
-    geometry="gridded",
+    geometry=geometry,
     split="validation",
-    subset="xs",
+    subset=subset,
     retrieval_input=inputs,
     batch_size=batch_size,
     target_config=target_config,
     stack=True,
-    ipwgml_path=ipwgml_path,
-    download=False,
-    shuffle=False
+    data_path=satrain_path,
+    download=False
 )
 
-training_loader = DataLoader(training_data, shuffle=True, batch_size=None, num_workers=4)
+training_loader = DataLoader(training_data, shuffle=True, batch_size=None, num_workers=1)
 validation_loader = DataLoader(validation_data, shuffle=False, batch_size=None)
 
+# Converte para numpy
 def load_limited(loader):
     X_list, y_surface_list, y_precip_mask_list, y_heavy_mask_list = [], [], [], []
 
     for x, y in loader:
         x = x.numpy()
         surface_precip = y["surface_precip"].numpy()
-        precip_mask = y["precip_mask"].numpy().astype(np.int32)  
-        heavy_precip_mask = y["heavy_precip_mask"].numpy().astype(np.int32)  
+        precip_mask = y["precip_mask"].numpy().astype(np.int32)
+        heavy_precip_mask = y["heavy_precip_mask"].numpy().astype(np.int32)
 
         X_list.append(x)
         y_surface_list.append(surface_precip)
@@ -71,13 +72,11 @@ def load_limited(loader):
 X_train, y_train_surface, y_train_precip_mask, y_train_heavy_mask = load_limited(training_loader)
 X_val, y_val_surface, y_val_precip_mask, y_val_heavy_mask = load_limited(validation_loader)
 
-def train_model(model_class, params, gpu_id, X_train, y_train, X_val, y_val):
-    print(f"Iniciando treinamento do modelo {model_class.__name__} na GPU {gpu_id}...")
-    model_params = params.copy()
-    model_params["gpu_id"] = gpu_id
-    model = model_class(**model_params)
+def train_model(model_class, params, X_train, y_train, X_val, y_val):
+    print(f"Iniciando treinamento do modelo {model_class.__name__} ...")
+    model = model_class(**params)
     model.fit(X_train, y_train, eval_set=[(X_val, y_val)], early_stopping_rounds=50, verbose=False)
-    print(f"Treinamento na GPU {gpu_id} finalizado.")
+    print(f"Treinamento finalizado para {model_class.__name__}.")
     return model
 
 params_reg = {
@@ -92,22 +91,11 @@ params_clf = {
     'reg_lambda': 9.5364, "tree_method": "gpu_hist", "objective": "binary:logistic", "random_state": 42
 }
 
-training_jobs = [
-    {"model_class": xgb.XGBRegressor, "params": params_reg, "y_train": y_train_surface, "y_val": y_val_surface, "name": "surface_precip"},
-    {"model_class": xgb.XGBClassifier, "params": params_clf, "y_train": y_train_precip_mask, "y_val": y_val_precip_mask, "name": "probability_of_precip"},
-    {"model_class": xgb.XGBClassifier, "params": params_clf, "y_train": y_train_heavy_mask, "y_val": y_val_heavy_mask, "name": "probability_of_heavy_precip"},
-]
+model_surface = train_model(xgb.XGBRegressor, params_reg, X_train, y_train_surface, X_val, y_val_surface)
+model_prob_precip = train_model(xgb.XGBClassifier, params_clf, X_train, y_train_precip_mask, X_val, y_val_precip_mask)
+model_prob_heavy = train_model(xgb.XGBClassifier, params_clf, X_train, y_train_heavy_mask, X_val, y_val_heavy_mask)
 
-trained_models = Parallel(n_jobs=3)( 
-    delayed(train_model)(
-        model_class=job["model_class"], params=job["params"], gpu_id=gpu_id,
-        X_train=X_train, y_train=job["y_train"], X_val=X_val, y_val=job["y_val"]
-    ) for gpu_id, job in enumerate(training_jobs)
-)
-
-model_surface, model_prob_precip, model_prob_heavy = trained_models
-
-class XGBMultiOutput(torch.nn.Module):
+class XGBoostOutput(torch.nn.Module):
     def __init__(self, models):
         super().__init__()
         self.models = models
@@ -120,14 +108,14 @@ class XGBMultiOutput(torch.nn.Module):
             "probability_of_heavy_precip": torch.from_numpy(self.models["probability_of_heavy_precip"].predict(x_np)).unsqueeze(-1).float(),
         }
 
-xgb_multi = XGBMultiOutput({
+model = XGBoostOutput({
     "surface_precip": model_surface,
     "probability_of_precip": model_prob_precip,
     "probability_of_heavy_precip": model_prob_heavy
 })
 
-wrapped = PytorchRetrieval(
-    model=xgb_multi,
+xgboost_retrieval = PytorchRetrieval(
+    model=model,
     retrieval_input=inputs,
     stack=True,
     device=torch.device("cuda"),
@@ -139,11 +127,12 @@ evaluator = Evaluator(
     domain="korea",
     geometry="gridded",
     retrieval_input=inputs,
-    download=True
+    data_path=satrain_path,
+    download=False
 )
 
 evaluator.evaluate(
-    retrieval_fn=wrapped,
+    retrieval_fn=xgboost_retrieval,
     input_data_format="tabular",
     batch_size=4048,
     n_processes=1
@@ -153,5 +142,9 @@ print("\nPrecipitation quantification")
 print(evaluator.get_precip_quantification_results(name="XGBOOST (GMI)").T.to_string())
 print("\nPrecipitation detection")
 print(evaluator.get_precip_detection_results(name="XGBOOST (GMI)").T.to_string())
+print("\nProbabilistic precipitation detection")
+print(evaluator.get_prob_precip_detection_results(name="XGBOOST (GMI)").T.to_string())
 print("\nHeavy precipitation detection")
 print(evaluator.get_heavy_precip_detection_results(name="XGBOOST (GMI)").T.to_string())
+print("\nHeavy probabilistic precipitation detection")
+print(evaluator.get_prob_heavy_precip_detection_results(name="XGBOOST (GMI)").T.to_string())
